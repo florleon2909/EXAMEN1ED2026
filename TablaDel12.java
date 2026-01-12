3cdb1809a2ff56af6b8af98b04cc0af54010dead
JUAN LEON <juanleon544452@hotmail.com>
2026-01-13 00:57:49 +0100

    primer comentario

--- TablaDel12.java
+++ TablaDel12.java
@@ -1,19 +1,19 @@
-public class TablaDel12 
+public class TablaDel12
 
-{ 
+{
 
-    public static void main(String args[]) 
+    public static void main(String args[])
 
-    { 
+    {
 
-        for(int i = 0; i <= 10; i++) 
+        for (int i = 0; i <= 10; i++)
 
-        { 
+        {
 
-            System.out.println("12 * "+ i + " = " + 12 * i + "\n"); 
+            System.out.println("12 * " + i + " = " + 12 * i + "\n");
+            System.out.println("Esta es la tabla del 12");
+        }
 
-        } 
+    }
 
-    } 
-
-} 
+}
