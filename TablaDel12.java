aacaf93b61d607bb2ee9e471c3367c7d038245af
JULIA LOPEZ <julialopez452221@hotmail.com>
2026-01-13 01:23:42 +0100

    flor, mensaje aplicacion maths

--- TablaDel12.java
+++ TablaDel12.java
@@ -12,7 +12,7 @@ public class TablaDel12
 
             System.out.println("12 * "+ i + " = " + 12 * i + "\n"); 
 
-            System.out.println("Desarrollado para la aplicación Maths.");
+            System.out.println("Desarrollado para la aplicación Maths");
 
         } 
 
